6a9f9192fccc1510673cabfbd4bcd5f3c372ffb7
kensukeohguchi1001 <kensuke.ohguchi@gmail.com>
2022-04-13 06:51:33 +0900

    pockerTestを追加

---
+++ src/lib/oop-poker/Game.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('Player.php');
+
+class Game
+{
+    public function __construct(private string $name)
+    {
+    }
+
+    public function start()
+    {
+        $player = new Player($this->name);
+
+        $cards = $player->drawCards();
+        return $cards;
+    }
+}
---
+++ src/lib/poker/PokerGame.php
@@ -0,0 +1,13 @@
+<?php
+
+class PokerGame
+{
+    public function __construct(private array $cards)
+    {
+
+    }
+    public function start(): array
+    {
+        return $this->cards;
+    }
+}
