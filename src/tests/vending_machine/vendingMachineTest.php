750d8c37210f2051df1b8200c7229f05e9f98fc0
kensukeohguchi1001 <kensuke.ohguchi@gmail.com>
2022-04-18 06:50:56 +0900

    ItemTest.phpを追加

--- src/tests/vending_machine/vendingMachineTest.php
+++ src/tests/vending_machine/vendingMachineTest.php
@@ -12,20 +12,28 @@ class VendingMachineTest extends TestCase
     $this->assertSame(0, $vendingMachine->depositCoin(0));
     $this->assertSame(0, $vendingMachine->depositCoin(150));
     $this->assertSame(100, $vendingMachine->depositCoin(100));
-    $this->assertSame(200, $vendingMachine->depositCoin(100));
-
   }
 
   public function testPressButton()
   {
+    $cider = new Item('cider');
+    $cola = new Item('cola');
     $vendingMachine = new VendingMachine();
 
     # お金が投入されてない場合は購入できない
-    $this->assertSame('', $vendingMachine->pressButton());
+    $this->assertSame('', $vendingMachine->pressButton($cider));
 
     // 100円を入れた場合はジュースを購入できる
     $vendingMachine->depositCoin(100);
-    $this->assertSame('cider', $vendingMachine->pressButton());
+    $this->assertSame('cider', $vendingMachine->pressButton($cider));
+
+    $vendingMachine->depositCoin(100);
+    $this->assertSame('', $vendingMachine->pressButton($cola));
+    // 投入金額が200円の場合はコーラを購入できる
+    $vendingMachine->depositCoin(100);
+    $this->assertSame('cola', $vendingMachine->pressButton($cola));
   }
+
+
 }
 // thisに入れていることで違うメソッドを呼び出せるメソッドを呼び出せる
