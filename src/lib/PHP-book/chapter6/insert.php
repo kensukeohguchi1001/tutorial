74053c0b4849d2a189eec3b3a15c52b0c0980386
kensukeohguchi1001 <kensuke.ohguchi@gmail.com>
2022-05-17 17:59:11 +0900

    トランザクションを追加

--- src/lib/PHP-book/chapter6/insert.php
+++ src/lib/PHP-book/chapter6/insert.php
@@ -1,32 +1,71 @@
 <?php
-    function connect(): PDO
-    {
-      $pdo = new PDO('mysql:host=db; dbname=book_log;', 'book_log', 'pass');
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-      return $pdo;
-    }
+function connect(): PDO
+{
+  $pdo = new PDO('mysql:host=db; dbname=book_log;', 'book_log', 'pass');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  return $pdo;
+}
+
+try {
+  $pdo = connect();
+  echo 'データベースへの接続に成功しました' . PHP_EOL;
+} catch (Exception $e) {
+  echo 'something wrong' . PHP_EOL;
+}
+
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>トランザクション</title>
+</head>
 
 <body>
+
   <?php
 
+  $pdo->beginTransaction();
+
   try {
-    $pdo = connect();
     $statement = $pdo->prepare('INSERT INTO reviews(title, author, status, score, summary, created_at)
         VALUES(:title, :author, :status, :score, :summary, CURRENT_TIMESTAMP)');
-        $statement->bindValue(':title','進撃の巨人', PDO::PARAM_STR);
-        $statement->bindValue(':author','isoyama', PDO::PARAM_STR);
-        $statement->bindValue(':status','done', PDO::PARAM_STR);
-        $statement->bindValue(':score', 100, PDO::PARAM_INT);
-        $statement->bindValue(':summary','perfect', PDO::PARAM_STR);
+    $statement->bindValue(':title', '進撃の巨人', PDO::PARAM_STR);
+    $statement->bindValue(':author', 'isoyama', PDO::PARAM_STR);
+    $statement->bindValue(':status', 'done', PDO::PARAM_STR);
+    $statement->bindValue(':score', 100, PDO::PARAM_INT);
+    $statement->bindValue(':summary', 'perfect', PDO::PARAM_STR);
 
-        echo 'insert done' . PHP_EOL;
-        $statement->execute();
-        $newId = $pdo->lastInsertId();
-        echo 'you already read' . $newId  .' books ';
-        } catch (PDOException $e) {
-          echo 'something wrong';
-        }
+    $statement->execute();
+    echo 'insert done' . PHP_EOL;
+
+    $statement = $pdo->prepare('transaction test');
+
+    $statement = $pdo->prepare('INSERT INTO reviews(title, author, status, score, summary, created_at)
+        VALUES(:title, :author, :status, :score, :summary, CURRENT_TIMESTAMP)');
+    $statement->bindValue(':title', '進撃の巨人', PDO::PARAM_STR);
+    $statement->bindValue(':author', 'isoyama', PDO::PARAM_STR);
+    $statement->bindValue(':status', 'done', PDO::PARAM_STR);
+    $statement->bindValue(':score', 100, PDO::PARAM_INT);
+    $statement->bindValue(':summary', 'perfect', PDO::PARAM_STR);
+
+    $statement->execute();
+    echo 'insert done2' . PHP_EOL;
+
+    $pdo->commit();
+    echo 'everything ok' . PHP_EOL;
+  } catch (Exception $e) {
+    $pdo->rollBack();
+    echo  'Exception was thrown, something wrong' . PHP_EOL;
+  }
+
   ?>
+
+
 </body>
+
+</html>
--- src/lib/PHP-book/chapter6/select/form.php
+++
@@ -1,7 +0,0 @@
-<body>
-    <h3>search for books title</h3>
-    <form name="search-form" action="results.php" method="get">
-        <input type="text" name="title" value="">
-        <button type="submit" name="operation" value="search">search</button>
-    </form>
-</body>
