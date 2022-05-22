d0d93d79fcd466e79f68630e01a07a5ad33f7831
kensukeohguchi1001 <kensuke.ohguchi@gmail.com>
2022-05-23 07:08:06 +0900

    スリーカードの処理を追加

--- src/lib/oop-poker/PokerThreeCardRule.php
+++ src/lib/oop-poker/PokerThreeCardRule.php
@@ -8,9 +8,37 @@ class PokerThreeCardRule implements PokerRule
       private const HIGH_CARD = 'high card';
       private const PAIR = 'pair';
       private const STRAIGHT = 'straight';
+      private const THREE_CARD = 'three card';
 
       public function getHand(array $cards)
       {
             $cardRanks = array_map(fn ($card) => $card->getRank(), $cards);
+            $name = self::HIGH_CARD;
+            if ($this->isStraight($cardRanks[0], $cardRanks[1], $cardRanks[2])) {
+                  $name = self::STRAIGHT;
+            } elseif ($this->isPair($cardRanks[0], $cardRanks[1], $cardRanks[2])) {
+                  $name = self::PAIR;
+            } else 
+            return $name;
       }
+
+      public function isStraight(int $card1, int $card2, int $card3): bool
+      {
+            return $card1 - $card2 === 1 && $card2 - $card3 === 1 || $this->isMinMax($card1, $card2, $card3);
+      }
+
+      public function isMinMax(int $card1, int $card2, int $card3): bool
+      {
+            return abs($card1 - $card2) === (max(Card::CARD_RANK) - min(Card::CARD_RANK)) && $card3 === 3;
+      }
+
+      public function isPair(int $card1, int $card2, int $card3): bool
+      {
+            return $card1 === $card2 !== $card3 || $card1 !== $card2 === $card3 || $card1 === $card3 !== $card2;
+      }
+      public function isThreeCard(int $card1, int $card2, int $card3): bool
+      {
+            return $card1 === $card2 === $card3;
+      }
+
 }
--- src/lib/oop-poker/PokerTwoCardRule.php
+++ src/lib/oop-poker/PokerTwoCardRule.php
@@ -5,36 +5,36 @@ require_once('PokerRule.php');
 
 class PokerTwoCardRule implements PokerRule
 {
-      private const HIGH_CARD = 'high card';
-      private const PAIR = 'pair';
-      private const STRAIGHT = 'straight';
+        private const HIGH_CARD = 'high card';
+        private const PAIR = 'pair';
+        private const STRAIGHT = 'straight';
 
-      public function getHand(array $cards): string
-      {
-          $cardRanks = array_map(fn ($card) => $card->getRank(), $cards);
-          $name = self::HIGH_CARD;
-          if ($this->isStraight($cardRanks[0], $cardRanks[1])) {
-              $name = self::STRAIGHT;
-          } elseif ($this->isPair($cardRanks[0], $cardRanks[1])) {
-              $name = self::PAIR;
-          }
-          return $name;
-      }
+        public function getHand(array $cards): string
+        {
+            $cardRanks = array_map(fn ($card) => $card->getRank(), $cards);
+            $name = self::HIGH_CARD;
+            if ($this->isStraight($cardRanks[0], $cardRanks[1])) {
+                $name = self::STRAIGHT;
+            } elseif ($this->isPair($cardRanks[0], $cardRanks[1])) {
+                $name = self::PAIR;
+            }
+            return $name;
+        }
 
-      public function isStraight(int $card1, int $card2): bool
-      {
-          return abs($card1 - $card2) === 1 || $this->isMinMax($card1, $card2);
+        public function isStraight(int $card1, int $card2): bool
+        {
+            return abs($card1 - $card2) === 1 || $this->isMinMax($card1, $card2);
 
-      }
+        }
 
-      public function isMinMax(int $card1, int $card2): bool
-      {
-          return abs($card1 - $card2) === (max(Card::CARD_RANK) - min(Card::CARD_RANK));
-      }
+        public function isMinMax(int $card1, int $card2): bool
+        {
+            return abs($card1 - $card2) === (max(Card::CARD_RANK) - min(Card::CARD_RANK));
+        }
 
-      public function isPair(int $card1, int $card2): bool
-      {
-          return $card1 === $card2;
-      }
+        public function isPair(int $card1, int $card2): bool
+        {
+            return $card1 === $card2;
+        }
 
 }
